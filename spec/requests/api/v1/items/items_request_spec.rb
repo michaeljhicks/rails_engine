c970525e37bb8e70119cd93df404c8c00c0e15dc
Michael Hicks <hicks.michael04@gmail.com>
2022-05-18 10:52:18 -0600

    test: it can update an item

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -74,7 +74,7 @@ require 'rails_helper'
    it 'can create an item' do
      merchant = create(:merchant)
      item_attributes = {
-       name: 'Michael Hicks Secret Aardvark Hot Sauce',
+       name: 'Michaels Secret Aardvark Hot Sauce',
        description: 'The Best Hot Sauce. Period.',
        unit_price: 15.99,
        merchant_id: merchant.id
@@ -90,5 +90,18 @@ require 'rails_helper'
      expect(created_item.description).to eq(item_attributes[:description])
      expect(created_item.unit_price).to eq(item_attributes[:unit_price])
      expect(created_item.merchant_id).to eq(item_attributes[:merchant_id])
+   end
+
+   it 'can update an item' do
+     id = create(:item).id
+     current_price = Item.last.unit_price
+     item_attributes_price = { unit_price: 20.99 }
+     headers = {'CONTENT_TYPE' => 'application/json'}
+
+     put "/api/v1/items/#{id}", headers: headers, params: JSON.generate(item_attributes_price)
+     item = Item.find_by(id: id)
+     expect(response).to be_successful
+     expect(item.unit_price).to_not eq(current_price)
+     expect(item.unit_price).to eq(20.99)
    end
  end
