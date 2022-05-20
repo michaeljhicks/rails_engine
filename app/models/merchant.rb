a835290f0517118f9455df3268fec02f42f5792d
Michael Hicks <hicks.michael04@gmail.com>
2022-05-20 07:13:00 -0600

    validate presence transaction and merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,11 @@
 class Merchant < ApplicationRecord
   has_many :items
-  has_many :invoices
-end 
+
+  validates_presence_of :name
+
+  def self.find_by_name(name)
+    where("lower(name) like ?", "%#{name.downcase}%")
+    .order(:name)
+    .limit(5)
+  end
+end
