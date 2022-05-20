62fb3546fda3c94b7c662166ae7a014f3c2c8f36
Michael Hicks <hicks.michael04@gmail.com>
2022-05-20 07:14:52 -0600

    add merchant_items_controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,27 +1,34 @@
 class Api::V1::ItemsController < ApplicationController
-
   def index
-    render json: ItemSerializer.new(Item.all)
+    render(json: ItemSerializer.new(Item.all))
   end
 
   def show
-    render json: ItemSerializer.new(Item.find(params[:id]))
+    render(json: ItemSerializer.new(Item.find(params[:id])))
   end
 
   def create
-    render json: ItemSerializer.new(Item.create(item_params))
+    item = Item.create(item_params)
+    if item.save
+      render(json: ItemSerializer.new(Item.create(item_params)), status: 201)
+    end
   end
 
   def update
-    render json: ItemSerializer.new(Item.update(item_params))
+    item = Item.update(params[:id], item_params)
+    if item.save
+      render(json: ItemSerializer.new(Item.update(params[:id], item_params)))
+    else
+      render :status => 404
+    end
   end
 
-  def destroy #when destroying an item, there is no JSON body, and it doesn't utilize a serializer
-    Item.destroy(params[:id]) #render status 201
+  def destroy
+    render(json: Item.find(params[:id]).destroy)
   end
 
   private
-    def item_params
-      params.permit(:name, :description, :unit_price, :merchant_id)
-    end
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
